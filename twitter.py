95c57eb06fe11b9907598a57c888a6f6321900ea
Kyle Burgess <90669224+kyleburgess2025@users.noreply.github.com>
2023-01-06 00:39:50 -0500

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -3,12 +3,14 @@ import requests
 import xml.etree.ElementTree as ET
 import os
 
+print(os.environ)
+
 def main():
     client = tweepy.Client(
-        consumer_key=os.environ["API_KEY"],
-        consumer_secret=os.environ["SECRET_KEY"],
-        access_token=os.environ["ACCESS_TOKEN"],
-        access_token_secret=os.environ["ACCESS_SECRET"]
+        consumer_key=os.getenv("API_KEY"),
+        consumer_secret=os.getenv("SECRET_KEY"),
+        access_token=os.getenv("ACCESS_TOKEN"),
+        access_token_secret=os.getenv("ACCESS_SECRET")
     )
 
     previous_atom = requests.get("https://squ1rrel.dev/atom.xml", allow_redirects=True).text
@@ -19,11 +21,11 @@ def main():
 
     if len(new_articles) > 0:
         if len(new_articles) == 1:
-            tweet_text = f'New writeup!\n{new_articles[0]}'
+            tweet_text = f'New writeup!\nhttps://squ1rrel.dev{new_articles[0]}'
         else:
             tweet_text = "New writeups!"
             for i in new_articles:
-                tweet_text += "\n" + i
+                tweet_text += "\nhttps://squ1rrel.dev" + i
         if len(tweet_text) > 280:
             tweet_text = "New writeups at https://squ1rrel.dev!"
         client.create_tweet(text=tweet_text)
